29e9659ae3c8d5571afc7775d210ca94190e63f5
Ngara-K <ngarakiringo@gmail.com>
2020-01-02 08:26:34 +0300

    0826EAT20200201

--- app/src/main/java/io/github/nwcafeteria/main.java
+++ app/src/main/java/io/github/nwcafeteria/main.java
@@ -1,23 +1,37 @@
 package io.github.nwcafeteria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import io.github.nwcafeteria.ui.categories_fragment;
+import io.github.nwcafeteria.ui.cities_fragment;
 import me.ibrahimsn.lib.OnItemReselectedListener;
 import me.ibrahimsn.lib.OnItemSelectedListener;
 import me.ibrahimsn.lib.SmoothBottomBar;
 
 public class main extends AppCompatActivity {
 
+    //configuring actionbar
+    private ActionBar toolbar;
     //smoothBar
-    SmoothBottomBar smoothBottomBar;
+    private SmoothBottomBar smoothBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting up toolbar
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("Cities");
+
+        //load first fragment
+        loadFragment(new cities_fragment());
 
         smoothBottomBar = findViewById(R.id.c_bottomBar);
 
@@ -25,7 +39,21 @@ public class main extends AppCompatActivity {
         smoothBottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelect(int i) {
-                Toast.makeText(main.this, "Selected position : " + i, Toast.LENGTH_SHORT).show();
+                Fragment fragment;
+
+                if (i==0) {
+                    toolbar.setTitle("Cities");
+                    fragment = new cities_fragment();
+                    loadFragment (fragment);
+                }
+                else  if (i==1) {
+                    toolbar.setTitle("Categories");
+                    fragment = new categories_fragment();
+                    loadFragment (fragment);
+                }
+                else {
+                    Toast.makeText(main.this, "No Action", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -35,5 +63,13 @@ public class main extends AppCompatActivity {
                 Toast.makeText(main.this, "Reselected position : " + i, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.c_frame, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
